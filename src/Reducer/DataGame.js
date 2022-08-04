323a9c2a6d831d507b7783ee7ea12e57fdb74267
Hudi Indrawan <79536179+Kororo212@users.noreply.github.com>
2022-08-05 01:12:58 +0700

    Update DataGame.js

--- src/Reducer/DataGame.js
+++ src/Reducer/DataGame.js
@@ -9,7 +9,7 @@ import axios from "axios"
 
 
 //data
-const API_KEY = process.env.KEY_API_RAWG
+const API_KEY = process.env.REACT_APP_RAWG_KEY
 export const  dataAsync = createAsyncThunk(
     'data/fetch', 
     async(page)=>{
